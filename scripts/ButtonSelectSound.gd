b4cd15a036160cb7057c3cf8baf69f7be715dfbb
alalfakawma <alalfakawma@live.com>
2020-03-28 22:21:18 +0530

    Button sounds leh Settings Menu Button

---
+++ scripts/ButtonSelectSound.gd
@@ -0,0 +1,4 @@
+extends AudioStreamPlayer2D
+
+func _play_sound():
+    self.play()
--- scripts/VBoxContainer.gd
+++ scripts/VBoxContainer.gd
@@ -3,8 +3,9 @@ extends VBoxContainer
 func _ready():
     var bg = self.get_parent().get_node("Background").rect_size
     var center = (bg / 2)
-    
+
     # Offset the position with the container height
     var vboxVec = self.rect_size - (self.rect_size / 2)
+    vboxVec.y -= 50
     self.set_position(center - vboxVec)
     pass
