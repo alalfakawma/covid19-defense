de6cc0be2f8770fa737dbe2fce3416f6d87a02c7
alalfakawma <alalfakawma@live.com>
2020-03-28 21:24:31 +0530

    Start, Exit Button (Exit theih tawh)

---
+++ scripts/VBoxContainer.gd
@@ -0,0 +1,10 @@
+extends VBoxContainer
+
+func _ready():
+    var bg = self.get_parent().get_node("Background").rect_size
+    var center = (bg / 2)
+    
+    # Offset the position with the container height
+    var vboxVec = self.rect_size - (self.rect_size / 2)
+    self.set_position(center - vboxVec)
+    pass
