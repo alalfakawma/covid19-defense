b1bc6342b9ef180209482681f898c9d77977d701
alalfakawma <alalfakawma@live.com>
2020-05-03 18:19:52 +0530

    Added readme

---
+++ readme.md
@@ -0,0 +1,4 @@
+#### Covid19 Defense
+###### Platform: Android
+###### Developers: Aseem and Jonathan
+###### Engine: [Godot](https://godotengine.org)
--- scripts/MissionComplete.gd
+++ scripts/MissionComplete.gd
@@ -2,7 +2,12 @@ extends Node2D
 
 func _next_mission():
     Game.currentMission += 1
-    get_tree().change_scene("res://scenes/Game.tscn")
+    # Check if next mission exists
+    if Game.currentMission >= Game.currentStageData.missions.size():
+        $Label.text = "Select Stage"
+        get_tree().change_scene("res://scenes/StageSelect.tscn")
+    else:
+        get_tree().change_scene("res://scenes/Game.tscn")
 
 func _main_menu():
     get_tree().change_scene("res://scenes/MainMenu.tscn")
