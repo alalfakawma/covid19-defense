0ef729d3030f80b21b5f7c2aa8c1201340d73ed6
alalfakawma <alalfakawma@live.com>
2020-07-29 20:53:42 +0530

    Upgrading now uses up your coins

--- scripts/Tower.gd
+++ scripts/Tower.gd
@@ -28,10 +28,11 @@ func _ready():
     $TowerUpgradeMenu/Label.text = "Price: %s" % Game.towerData[tier][level + 1].price
 
 func _process(delta):
-    if Game.towerData[tier][level + 1].price > Game.coins:
-        $TowerUpgradeMenu/Tick.disabled = true
-    else:
-        $TowerUpgradeMenu/Tick.disabled = false
+    if level != 1:
+        if Game.towerData[tier][level + 1].price > Game.coins:
+            $TowerUpgradeMenu/Tick.disabled = true
+        else:
+            $TowerUpgradeMenu/Tick.disabled = false
     
     if currentTarget != null:
         # Look at the target
@@ -87,6 +88,7 @@ func _upgrade():
         $TowerUpgradeMenu.visible = true
 
 func _upgrade_confirmed():
+    Game.coins -= Game.towerData[tier][level + 1].price    
     set_tower(tier, level + 1)
     $TowerUpgradeMenu.visible = false
 
